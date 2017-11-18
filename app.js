ed881d47a2291382b457fcc1272e8bc48a52a7d0
siwasu17 <siwasu17@gmail.com>
2017-11-18 23:23:26 +0900

    Fix indent

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const flower = require("flower-power");
 let myip;
 const os = require("os");
 os.networkInterfaces()["en0"].forEach(function(nic){
-	if(nic["family"] == "IPv4"){
-		myip = nic["address"];
-	}
+  if(nic["family"] == "IPv4"){
+    myip = nic["address"];
+  }
 });
 
 //ロガー準備
@@ -34,88 +34,88 @@ const LIGHT_THRESHOLD = 0.3;
 
 //夜の時間帯か判定
 const isNight = function(currentMoment){
-	logger.info(currentMoment);
-	let nightStart = moment({hours: 20, minutes: 00});
-	let nightEnd = moment({hours: 23, minutes: 30});
+  logger.info(currentMoment);
+  let nightStart = moment({hours: 20, minutes: 00});
+  let nightEnd = moment({hours: 23, minutes: 30});
 
-	if(currentMoment.diff(nightStart) > 0 && currentMoment.diff(nightEnd) < 0){
-		return true;
-	}
-	return false;
+  if(currentMoment.diff(nightStart) > 0 && currentMoment.diff(nightEnd) < 0){
+    return true;
+  }
+  return false;
 }
 
 
 //一定間隔毎に起動チェックして、条件を満たしたらセンサーを動かし始める
 const interval = 5000;
 setInterval(function() {
-	logger.info("起動チェック中...");
-	let current = moment();
-	//夜でなければすぐ終了
-	if(!isNight(current) ){
-		logger.info("まだ夜ではありません zzz");
-		return;
-	}
+  logger.info("起動チェック中...");
+  let current = moment();
+  //夜でなければすぐ終了
+  if(!isNight(current) ){
+    logger.info("まだ夜ではありません zzz");
+    return;
+  }
 
-	//すでに今日の動作は終わっているか
-	if(current.format("YYYYMMDD") in greatedDate){
-		logger.info("今日はもう挨拶しました");
-		return;
-	}
+  //すでに今日の動作は終わっているか
+  if(current.format("YYYYMMDD") in greatedDate){
+    logger.info("今日はもう挨拶しました");
+    return;
+  }
 
-	//先行したプロセスが動いているか確認
-	if(alreadyRunning){
-		logger.info("すでに起動しています");
-		return;
-	}
+  //先行したプロセスが動いているか確認
+  if(alreadyRunning){
+    logger.info("すでに起動しています");
+    return;
+  }
 
-	logger.info("起動開始");
-	alreadyRunning = true;
-	logger.info("FlowerPowerを探しています...");
+  logger.info("起動開始");
+  alreadyRunning = true;
+  logger.info("FlowerPowerを探しています...");
 
-	flower.discover((fp) => {
+  flower.discover((fp) => {
 
-		logger.info("FlowerPowerを見つけました。接続中です...");
+    logger.info("FlowerPowerを見つけました。接続中です...");
 
-		fp.connectAndSetup((err) => {
-			if(err) return;
+    fp.connectAndSetup((err) => {
+      if(err) return;
 
-			logger.info("接続しました");
+      logger.info("接続しました");
 
-			fp.enableLiveMode((err) => {
-				if(err)return;
+      fp.enableLiveMode((err) => {
+        if(err)return;
 
-				logger.info("ライブモードを開始します");
-				isLiveMode = true;
+        logger.info("ライブモードを開始します");
+        isLiveMode = true;
 
-				//照度センサーを監視して、明るくなったら動作する
-				fp.on("sunlightChange", (sunlight) => {
-					logger.info("照度; " + sunlight);
+        //照度センサーを監視して、明るくなったら動作する
+        fp.on("sunlightChange", (sunlight) => {
+          logger.info("照度; " + sunlight);
 
-					//ライブモード中に夜時間帯を越えていたら終了する
-					if(!isNight(moment())){
-						logger.info("指定の時間帯を越えたので終了します");
-						alreadyRunning = false;
-						fp.disconnect((err) => { return; });
-					}
+          //ライブモード中に夜時間帯を越えていたら終了する
+          if(!isNight(moment())){
+            logger.info("指定の時間帯を越えたので終了します");
+            alreadyRunning = false;
+            fp.disconnect((err) => { return; });
+          }
 
-					if(sunlight > LIGHT_THRESHOLD){
-						if(myip){
-							let text = "おかえりなさい。今日もおつかれさまでした。";
-							googlehome.play("http://" + myip + ":8080/audio/" + text, (notifyRes) => {
-								logger.info(notifyRes);
-							});
-							greatedDate[moment().format("YYYYMMDD")] = 1;
+          if(sunlight > LIGHT_THRESHOLD){
+            if(myip){
+              let text = "おかえりなさい。今日もおつかれさまでした。";
+              googlehome.play("http://" + myip + ":8080/audio/" + text, (notifyRes) => {
+                logger.info(notifyRes);
+              });
+              greatedDate[moment().format("YYYYMMDD")] = 1;
 
-							//終了
-							logger.info("ライブモードを終了します");
-							fp.disableLiveMode((err) => {
-								fp.disconnect((err) => { return; });
-								return;
-						  });
-						}
-					}
-				});
-			});
-		}); 
-	});
+              //終了
+              logger.info("ライブモードを終了します");
+              fp.disableLiveMode((err) => {
+                fp.disconnect((err) => { return; });
+                return;
+              });
+            }
+          }
+        });
+      });
+    }); 
+  });
 }, interval);
